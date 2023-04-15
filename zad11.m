d826b11496d3d2d6c840d14caefdaaf7432b34d8
Matúš <xmachatam@stuba.sk>
2023-04-15 16:13:04 +0200

    pravil som tie ulohy a,b,c. kukni ci to je dobre, este zistim co treba vyplotovat a zacnem pisat dokumentaciu

---
+++ zad11.m
@@ -0,0 +1,43 @@
+clc;
+clear;
+
+load("out11.mat");
+data_wanted = [29.3 33.5 38 42 46.5 49.5 52.5 54 54.5 55 55];
+data =        [29 33 37 41 45 47.5 50 51.3 51.6 52 52];
+e = data_wanted - data;
+u = [0 10 20 30 40 50 60 70 80 90 100]';
+l=ones(size(u)); % jednotkovy vektor
+
+% a) ----
+
+h1=[l,u]; % matica H
+theta1=inv(h1'*h1);
+theta2 =theta1*h1';
+theta3 = theta2*data'; % odhad parametrov
+y1=h1*theta3;   % vektor vystupov modelu
+% e=y-y1;   % vektor odchylok
+q1=e'*e;      % suma kvadratov odchylok 
+
+% b) ----
+
+u2 = u.^2;
+h2 = [l,u,u2];
+theta_kvadr=inv(h2'*h2)*h2'*data'; % odhad parametrov 
+y2 = h2 * theta_kvadr;
+
+% c) ----
+
+h3 = [l,sqrt(u)];
+theta_odmoc = theta3;
+y3 = h3 * theta_odmoc;
+
+
+% vykreslenie grafu ----
+
+% x= out.tout;
+% y_graph= out.ScopeData.signals.values;
+% y2_graph= out.ScopeData.signals(2).values;
+% % plot (x,y_graph)
+% plot (x,y2_graph)
+% ylabel("Temperature [°C]")
+% xlabel("Time [s]")
